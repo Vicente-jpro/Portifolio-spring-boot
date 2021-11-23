7bac6f41202a08eefcdebf7b8682d5aca000d7e8
Vicente-jpro <vicenteviciii@outlook.com>
2021-11-24 00:48:14 +0100

    Update StudentController.java

--- src/main/java/com/vicenteportfilo/controllers/StudentController.java
+++ src/main/java/com/vicenteportfilo/controllers/StudentController.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.vicenteportfilo.domains.Student;
+import com.vicenteportfilo.exceptions.StudentDeleteException;
+import com.vicenteportfilo.exceptions.StudentNotFoundException;
 import com.vicenteportfilo.service.StudendServicedb;
 import com.vicenteportfilo.service.StudentService;
 
@@ -19,6 +22,11 @@ public class StudentController {
 	
 	@GetMapping("/")
 	public String index(Model model) {
+		return "index";
+	}
+	
+	@GetMapping("/new-student")
+	public String formToSave(Model model) {
 		model.addAttribute("saveStudent", new Student());
 		return "student_save";
 	}
@@ -29,8 +37,25 @@ public class StudentController {
 		return "redirect:/view-students";
 	}
 	
+	@GetMapping("/update-student/{id}")
+	public String update(@PathVariable Long id, Model model) throws StudentNotFoundException, StudentDeleteException {
+		
+		Student student = this.studentServicedb.getStudent( new Student( id) );
+		model.addAllAttributes("updateStudent", student );
+		return "redirect:/view-students";
+	}
+	
+	@GetMapping("/delete-student/{id}")
+	public String delete(@PathVariable Long id  ) throws StudentNotFoundException, StudentDeleteException {
+		this.studentServicedb.delete( new Student( id) );
+		return "redirect:/view-students";
+	}
+	
 	@GetMapping("/view-students")
 	public String view(Model model) {
+		model.addAttribute("listStudent", this.studentServicedb.read());
 		return "student_view";
 	}
+	
+
 }
