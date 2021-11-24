2f6c0abacb6eda97428226b8729072363bf7518c
Vicente-jpro <vicenteviciii@outlook.com>
2021-11-24 21:19:36 +0100

    Save validation
    Save validation using js on client side

---
+++ src/main/resources/static/js/validation.js
@@ -0,0 +1,13 @@
+function validationSave() {
+	let name = $('#name').val();
+	let email = $('#email').val();
+	if (name === '') {
+		alert('Name can not be null');
+		return false;
+	} else if (email === '') {
+		alert('Email can not be null')
+		return false;
+	}
+	return true;
+}
+
