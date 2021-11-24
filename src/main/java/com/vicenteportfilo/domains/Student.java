be709447661d7a9cccc13494830ef33a0ca8d0aa
Vicente-jpro <vicenteviciii@outlook.com>
2021-11-24 02:54:06 +0100

    Removed files unused

--- src/main/java/com/vicenteportfilo/domains/Student.java
+++ src/main/java/com/vicenteportfilo/domains/Student.java
@@ -6,11 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy.Strategy;
 
 @Entity
 @Table(name = "students")
@@ -29,41 +24,6 @@ public class Student {
 	public Student(Long id) {
 		this.id = id;
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
-
-	public Student() {
-
-	}
-
-	public Student(Long id) {
-		this.id = id;
-	}
-
 
 	public Long getId() {
 		return id;
