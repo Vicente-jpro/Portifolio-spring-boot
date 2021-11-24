f93f8e8bd8755fc84fc9c6d27ca08125eb1cc781
Vicente-jpro <vicenteviciii@outlook.com>
2021-11-24 02:41:30 +0100

    Dupicated methods
    Dupicated getters and setters removed

--- src/main/java/com/vicenteportfilo/domains/Student.java
+++ src/main/java/com/vicenteportfilo/domains/Student.java
@@ -6,10 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy.Strategy;
 
 @Entity
@@ -55,38 +51,5 @@ public class Student {
 	}
 	
 	
-
-	public Student() {
-
-	}
-
-	public Student(Long id) {
-		this.id = id;
-	}
-
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
 }
