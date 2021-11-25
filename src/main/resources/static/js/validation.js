1aa23d13816881056ee478144dc573b582fb05c0
Vicente-jpro <vicenteviciii@outlook.com>
2021-11-25 20:48:40 +0100

    Validation
    I used the jquery validation pluggin to validate forms

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -1,18 +1,2 @@
-
-	
-	function validationSave() {
-
-	let name = $('#name').val();
-	let email = $('#email').val();
-	
-	if (name === '' ) {
-		alert('Name can not be null');
-		return false;
-	} else if (email === '' ){
-		alert('Email can not be null')
-		return false;
-	}
-	
-	return true;
-}
-
+$('#saveForm').validate();
+$('#saveForm').validate();
